4cd6993e96bd6cae763fd5eb99e9b6be483af1c0
Franco Vega <vegafranco97@gmail.com>
2019-09-27 23:45:44 -0300

    Debería poder leer archivos 1 y 2. Prueba pendiente

--- FUSE/src/SAC-cli.c
+++ FUSE/src/SAC-cli.c
@@ -21,27 +21,72 @@
 
 //va a ser llamada cuando el sistema pida los atributos de un archivo
 
-static int hola_getattr( const char *path, struct stat *st )
+static int hacer_getattr( const char *path, struct stat *st )
 {
 	st->st_uid = getuid();			//el duenio del archivo
 	st->st_gid = getgid();			//el mismo?
-	st->st_atime = time( NULL );	// last access time
-	st->st_mtime = time( NULL );	// last modification time
+	st->st_atime = time( NULL );	// last "A"ccess time
+	st->st_mtime = time( NULL );	// last "M"odification time
 
-	if ( strcmp( path, "/" ) == 0 )	//path punto de montaje
+	if ( strcmp( path, "/" ) == 0 )	//si path es el punto de montaje
 	{
-		st->st_mode = S_IFDIR | 0755; //0755 son los bits de permiso
-		st->st_nlink = 2;
+		st->st_mode = S_IFDIR | 0755; //bits de permiso
+		st->st_nlink = 2;		//num de hardlinks
 	}
 	else
 	{
 		st->st_mode = S_IFREG | 0644;
 		st->st_nlink = 1;
-		st->st_size = 1024;
+		st->st_size = 1024;		//tamanio de los archivos
 	}
 	return 0;
 }
 
+//hace una lista de los archivos o directorios disponibles en una ruta específica
+
+static int hacer_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
+{
+	printf( "... Consiguiendo la lista de archivos de %s\n", path );
+
+	filler( buffer, ".", NULL, 0 ); // llenamos la lista de directorios con el directorio actual
+	filler( buffer, "..", NULL, 0 ); // parent
+
+	if ( strcmp( path, "/" ) == 0 )
+	{
+		filler( buffer, "archivo1", NULL, 0 );	//agregamos los archivos
+		filler( buffer, "archivo2", NULL, 0 );
+	}
+
+	return 0;
+}
+
+//lee el contenido de un archivo. Retorna el número de bytes que fueron leidos correctamente
+
+static int hacer_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
+{
+	printf( "... Leyendo %s, %u, %u\n", path, offset, size );
+
+	char archivo1Text[] = "Archivo 1 presente";
+	char archivo2Text[] = "El 2 tambien";
+	char *selectedText = NULL;
+
+	if ( strcmp( path, "/archivo1" ) == 0 )		//si es el archivo 1 se asigna su puntero al texto seleccionado
+		selectedText = archivo1Text;
+	else if ( strcmp( path, "/archivo2" ) == 0 )
+		selectedText = archivo2Text;
+	else
+		return -1;
+
+	memcpy( buffer, selectedText + offset, size );	//copia a buffer el contenido del archivo
+
+	return strlen( selectedText ) - offset;
+}
+
+static struct fuse_operations operaciones = {
+    .getattr	= hacer_getattr,
+    .readdir	= hacer_readdir,
+    .read		= hacer_read,
+};
 
 void levantarServidorFUSE()
 {
@@ -89,7 +134,9 @@ void levantarConfig()
 	loggearInfoServidor(ip,puerto);
 }
 
-int main(void) {
+int main( int argc, char *argv[] )
+{
+	/*
 	remove("Linuse.log");
 	iniciarLog("FUSE");
 	loggearInfo("Se inicia el proceso FUSE...");
@@ -97,6 +144,8 @@ int main(void) {
 	//levantarServidorFUSE();
 	liberarVariablesGlobales();
 	return EXIT_SUCCESS;
+	*/
+	return fuse_main( argc, argv, &operaciones, NULL );
 }
 
 void liberarVariablesGlobales()
