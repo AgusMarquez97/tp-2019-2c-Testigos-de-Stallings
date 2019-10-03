f4104ef5468f2703ddec906ef0c4721bdc87c08a
Franco Vega <vegafranco97@gmail.com>
2019-10-03 13:04:30 -0300

    Agregadas mkdir, mknod y write. Modificaciones a codigo anterior

--- FUSE/src/SAC-cli.c
+++ FUSE/src/SAC-cli.c
@@ -19,73 +19,172 @@
 #include <time.h>
 #include "FUSE.h"
 
+/*NOTA: Por el momento solo puede crear archivos y directorios en el punto de montaje por simplicidad
+ * Mas adelante va a tener que ser modificado*/
+
+//arrays que almacenan los directorios, archivos y sus contenidos. A ser cambiado por algo mas lindo mas adelante
+char listaDirectorios[256][256];
+int indiceDirActual = -1;
+
+char listaArchivos[256][256];
+int indiceArchActual = -1;
+
+char listaContenido[256][256];
+int indiceContActual = -1;
+
+//agrega el nombre de un directorio a la lista. No el directorio completo
+void agregarDirectorio(const char *nombreDir)
+{
+	indiceDirActual++;
+	strcpy(listaDirectorios[indiceDirActual],nombreDir);
+}
+
+//devuelve 1 si el directorio existe, 0 si no existe
+int existeDirectorio(const char *path)
+{
+	path++; // Elimina el "/"
+
+	for (int indActual = 0; indActual <= indiceDirActual; indActual++)
+		if (strcmp(path,listaDirectorios[indActual]) == 0)
+			return 1;
+
+	return 0;
+}
+
+//agrega un archivo a la lista y setea su contenido. Ambos indices se tienen que mover juntos
+void agregarArchivo(const char *nombre)
+{
+	indiceArchActual++;
+	strcpy(listaArchivos[indiceArchActual], nombre);
+
+	indiceContActual++;
+	strcpy(listaContenido[indiceContActual], "");
+}
+
+//devuelve 1 si el archivo existe, 0 si no existe
+int existeArchivo(const char *path)
+{
+	path++; // Elimina el "/"
+
+	for (int indActual = 0; indActual <= indiceArchActual; indActual++)
+		if (strcmp(path, listaArchivos[indActual]) == 0)
+			return 1;
+
+	return 0;
+}
+
+//retorna el indice del archivo. -1 si no existe
+int indiceArchivo(const char *path)
+{
+	path++; // Elimina el "/"
+
+	for (int indActual = 0; indActual <= indiceArchActual; indActual++)
+		if (strcmp(path,listaArchivos[indActual]) == 0)
+			return indActual;
+
+	return -1;
+}
+
+//busca el indice del archivo y copia el nuevo contenido. Si no existe el archivo, no hace nada
+void escribirEnArchivo(const char *path, const char *contenido)
+{
+	int indArch = indiceArchivo(path);
+
+	if (indArch == -1)
+		return;
+
+	strcpy(listaContenido[indArch], contenido);
+}
 //va a ser llamada cuando el sistema pida los atributos de un archivo
 
-static int hacer_getattr( const char *path, struct stat *st )
+static int hacer_getattr(const char *path, struct stat *st)
 {
-	st->st_uid = getuid();			//el duenio del archivo
-	st->st_gid = getgid();			//el mismo?
-	st->st_atime = time( NULL );	// last "A"ccess time
-	st->st_mtime = time( NULL );	// last "M"odification time
+	st->st_uid = getuid();		//el duenio del archivo
+	st->st_gid = getgid();		//el mismo?
+	st->st_atime = time(NULL);	//last "A"ccess time
+	st->st_mtime = time(NULL);	//last "M"odification time
 
-	if ( strcmp( path, "/" ) == 0 )	//si path es el punto de montaje
+	if (strcmp(path, "/") == 0 || existeDirectorio(path) == 1)//si path es el punto de montaje o existe el directorio
 	{
 		st->st_mode = S_IFDIR | 0755; //bits de permiso
 		st->st_nlink = 2;		//num de hardlinks
 	}
-	else
+	else if (existeArchivo(path) == 1)//si es un archivo
 	{
 		st->st_mode = S_IFREG | 0644;
 		st->st_nlink = 1;
 		st->st_size = 1024;		//tamanio de los archivos
+	}
+	else //no existe en el filesystem
+	{
+		return -ENOENT;
 	}
 	return 0;
 }
 
 //hace una lista de los archivos o directorios disponibles en una ruta específica
 
-static int hacer_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
+static int hacer_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
-	printf( "... Consiguiendo la lista de archivos de %s\n", path );
+	printf("... Consiguiendo la lista de archivos de %s\n", path);
 
-	filler( buffer, ".", NULL, 0 ); // llenamos la lista de directorios con el directorio actual
-	filler( buffer, "..", NULL, 0 ); // parent
+	filler(buffer, ".", NULL, 0); // llenamos la lista de directorios con el directorio actual
+	filler(buffer, "..", NULL, 0); // parent
 
-	if ( strcmp( path, "/" ) == 0 )
+	if (strcmp(path, "/" ) == 0)
 	{
-		filler( buffer, "archivo1", NULL, 0 );	//agregamos los archivos
-		filler( buffer, "archivo2", NULL, 0 );
+		for (int indActual = 0; indActual <= indiceDirActual; indActual++)
+			filler( buffer, listaDirectorios[indActual], NULL, 0);
+		for (int indActual = 0; indActual <= indiceArchActual; indActual++)
+			filler( buffer, listaArchivos[indActual], NULL, 0);
 	}
 
 	return 0;
 }
 
 //lee el contenido de un archivo. Retorna el número de bytes que fueron leidos correctamente
-
-static int hacer_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
+static int hacer_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-	printf( "... Leyendo %s, %u, %u\n", path, offset, size );
-
-	char archivo1Text[] = "Archivo 1 presente";
-	char archivo2Text[] = "El 2 tambien";
-	char *selectedText = NULL;
-
-	if ( strcmp( path, "/archivo1" ) == 0 )		//si es el archivo 1 se asigna su puntero al texto seleccionado
-		selectedText = archivo1Text;
-	else if ( strcmp( path, "/archivo2" ) == 0 )
-		selectedText = archivo2Text;
-	else
+	printf("... Leyendo %s, %u, %u\n", path, offset, size);
+	int indArch = indiceArchivo(path);
+	if (indArch == -1)
 		return -1;
+	char *contenido = listaContenido[indArch];
+	memcpy(buffer, contenido + offset, size);
+	return strlen(contenido) - offset;
+}
 
-	memcpy( buffer, selectedText + offset, size );	//copia a buffer el contenido del archivo
+//va a ser llamada para crear un nuevo directorio. "modo" son los bits de permiso, ver documentacion de mkdir
+static int hacer_mkdir(const char *path, mode_t modo)
+{
+	path++;
+	agregarDirectorio(path);
+	return 0;
+}
 
-	return strlen( selectedText ) - offset;
+//va a ser llamada para crear un nuevo archivo. "dispositivo" tiene que ser especificada si el nuevo archivo
+//es un dispositivo, ver documentacion de mknod
+static int hacer_mknod(const char *path, mode_t modo, dev_t dispositivo)
+{
+	path++;
+	agregarArchivo(path);
+	return 0;
+}
+
+//va a ser llamada para escribir contenido en un archivo
+static int hacer_write(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *info)
+{
+	escribirEnArchivo(path, buffer);
+	return size;
 }
 
 static struct fuse_operations operaciones = {
     .getattr	= hacer_getattr,
     .readdir	= hacer_readdir,
     .read		= hacer_read,
+	.mkdir		= hacer_mkdir,
+	.mknod		= hacer_mknod,
+	.write		= hacer_write,
 };
 
 void levantarServidorFUSE()
@@ -145,7 +244,8 @@ int main( int argc, char *argv[] )
 	liberarVariablesGlobales();
 	return EXIT_SUCCESS;
 	*/
-	return fuse_main( argc, argv, &operaciones, NULL );
+	return fuse_main(argc, argv, &operaciones, NULL);
+	//al correrlo pasarle como parametro la direccion donde lo quieras montar
 }
 
 void liberarVariablesGlobales()
