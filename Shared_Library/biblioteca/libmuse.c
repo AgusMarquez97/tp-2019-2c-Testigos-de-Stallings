4442c66a8f24ea4de909def520974f1aabef686d
AgusMarquez97 <aguusmarquez@gmail.com>
2019-12-18 01:28:22 -0300

    + cambios de logs

--- Shared_Library/biblioteca/libmuse.c
+++ Shared_Library/biblioteca/libmuse.c
@@ -1,7 +1,7 @@
 #include "libmuse.h"
 
 int muse_init(int id, char* ip, int puerto) {
-
+	char aux[150];
 	remove("Linuse.log");
 	iniciarLog("Linuse");
 
@@ -21,33 +21,43 @@ int muse_init(int id, char* ip, int puerto) {
 		recibirInt(socketCliente, &retorno);
 
 		if(retorno == -1) //@return: error -1 / ok 0
-		loggearInfo("Fallo al realizar el handshake, init duplicado");
+		{
+		sprintf(aux,"Fallo al realizar el handshake, init duplicado para el proceso %d",id_muse);
+		loggearInfo(aux);
+		}
 		else
-		loggearInfo("Biblioteca MUSE inicializada con éxito");
+		{
+		sprintf(aux,"Biblioteca MUSE inicializada con éxito para el proceso %d",id_muse);
+		loggearInfo(aux);
+		}
 
 		close(socketCliente);
 	} else {
-		loggearWarning("No se ha podido inicializar la biblioteca MUSE");
+		sprintf(aux,"No se ha podido inicializar la biblioteca MUSE para el proceso %d",id_muse);
+		loggearWarning(aux);
 	}
 	return retorno;
 
 }
 
 uint32_t muse_alloc(uint32_t tam) {
+	char aux[170];
 	uint32_t direccionMemoria = 0;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
 	if(socketCliente != -1) {
-		loggearInfo("Reservando porción de memoria...");
+		//loggearInfo("Reservando porción de memoria...");
 		pthread_mutex_lock(&mutex_malloc);
 		enviarMalloc(socketCliente, id_muse, (int32_t)tam);
 		pthread_mutex_unlock(&mutex_malloc);
 		recibirUint(socketCliente, &direccionMemoria); //@return: error [0] / ok [dirección de memoria]
 		close(socketCliente);
 		if(direccionMemoria != 0) {
-			loggearInfo("Porción de memoria reservada con éxito");
+			sprintf(aux,"Porción de memoria %u reservada con éxito en la direccion %u para el proceso %d",tam,direccionMemoria,id_muse);
+			loggearInfo(aux);
 		} else {
-			loggearWarning("No se ha podido reservar la porción de memoria solicitada");
+			sprintf(aux,"No se han podido reservar %u bytes para el proceso %d",tam,id_muse);
+			loggearWarning(aux);
 		}
 	}
 
@@ -57,19 +67,22 @@ uint32_t muse_alloc(uint32_t tam) {
 }
 
 void muse_free(uint32_t dir) {
+	char aux[150];
 	int32_t retorno = -1;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
 	if(socketCliente != -1) {
-		loggearInfo("Liberando porción de memoria...");
-			enviarFree(socketCliente, id_muse, dir);
-			recibirInt(socketCliente, &retorno);
-			close(socketCliente);
+		//loggearInfo("Liberando porción de memoria...");
+		enviarFree(socketCliente, id_muse, dir);
+		recibirInt(socketCliente, &retorno);
+		close(socketCliente);
 		//Para mas adelante: Tenemos que definir un codigo de error que nos indique la razon por la que no se pudo realizar el free (y levantar raise si hace falta)
 		if(retorno != 0) {
-			loggearInfo("Porción de memoria liberada con éxito");
+			sprintf(aux,"No se ha podido liberar la porción de memoria solicitada %u liberada con éxito para el proceso %d",dir,id_muse);
+			loggearInfo(aux);
 		} else {
-			loggearWarning("No se ha podido liberar la porción de memoria solicitada");
+			sprintf(aux,"No se ha podido liberar la posicion de memoria %u liberada con éxito para el proceso %d",dir,id_muse);
+			loggearWarning(aux);
 		}
 	}
 
@@ -77,20 +90,21 @@ void muse_free(uint32_t dir) {
 
 int muse_get(void* dst, uint32_t src, size_t n) {
 	int32_t retorno = -1;
-	char aux[80];
+	char aux[150];
 
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 	void* buffer;
 
 	if(socketCliente != -1) {
-		loggearInfo("Recuperando información desde memoria...");
+		//loggearInfo("Recuperando información desde memoria...");
 		enviarGet(socketCliente, id_muse, src, (int32_t)n);
 		retorno = recibirVoid(socketCliente, &buffer); //@return: error [-1] / ok [0]
 		close(socketCliente);
 		if(retorno != -1) {
 				memcpy(dst, buffer, n);
 				free(buffer);
-				loggearInfo("Información recuperada desde memoria con éxito");
+				sprintf(aux,"%lu bytes de la posicion %u recuperada con éxito para el proceso %d",n,src,id_muse);
+				loggearInfo(aux);
 			}
 		else
 		{
@@ -105,17 +119,18 @@ int muse_get(void* dst, uint32_t src, size_t n) {
 }
 
 int muse_cpy(uint32_t dst, void* src, int n) {
-	char aux[80];
+	char aux[150];
 	int32_t retorno = -1;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
 	if(socketCliente != -1) {
-		loggearInfo("Copiando datos en Memoria...");
+		//loggearInfo("Copiando datos en Memoria...");
 		enviarCpy(socketCliente, id_muse, dst, src, (int32_t)n);
 		recibirInt(socketCliente, &retorno); //@return: error [-1] / ok [0]
 		close(socketCliente);
 			if(retorno != -1) {
-					loggearInfo("Datos en memoria copiados con éxito");
+					sprintf(aux,"%d bytes copiados en memoria con éxito en la posicion %u para el proceso %d",n,dst,id_muse);
+					loggearInfo(aux);
 				} else {
 					sprintf(aux,"Direccion %u genera en segmentation fault para el proceso %d",dst,id_muse);
 					loggearWarning(aux);
@@ -128,6 +143,7 @@ int muse_cpy(uint32_t dst, void* src, int n) {
 }
 
 uint32_t muse_map(char* path, size_t length, int flags) {
+	char aux[200];
 	uint32_t direccionMemoria = 0;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
@@ -141,9 +157,11 @@ uint32_t muse_map(char* path, size_t length, int flags) {
 		recibirUint(socketCliente, &direccionMemoria); //@return: error [0] / ok [dirección de memoria]
 		close(socketCliente);
 		if(direccionMemoria != 0) {
-				loggearInfo("Información mappeada con éxito");
+				sprintf(aux,"%lu bytes del archivo %s mappeado con exito en la direccion %u para el proceso %d",length,path,direccionMemoria,id_muse);
+				loggearInfo(aux);
 			} else {
-				loggearWarning("No se ha podido mappear la información");
+				sprintf(aux,"No se ha podido mappear el archivo %s para el proceso %d",path,id_muse);
+				loggearWarning(aux);
 			}
 	}
 
@@ -153,7 +171,7 @@ uint32_t muse_map(char* path, size_t length, int flags) {
 }
 
 int muse_sync(uint32_t addr, size_t len) {
-
+	char aux[200];
 	int32_t retorno = -1;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
@@ -162,9 +180,11 @@ int muse_sync(uint32_t addr, size_t len) {
 		recibirInt(socketCliente, &retorno); //@return: error [-1] / ok [0]
 		close(socketCliente);
 		if(retorno != -1) {
-				loggearInfo("Información sincronizada con éxito");
+				sprintf(aux,"%lu bytes de %u sincronizada con éxito para el proceso %d",len,addr,id_muse);
+				loggearInfo(aux);
 			} else {
-				loggearWarning("No se ha podido sincronizar la información");
+				sprintf(aux,"No se ha podido sincronizar la información en la posicion %u para el proceso %d",addr,id_muse);
+				loggearWarning(aux);
 			}
 	}
 
@@ -173,7 +193,7 @@ int muse_sync(uint32_t addr, size_t len) {
 }
 
 int muse_unmap(uint32_t dir) {
-
+	char aux[170];
 	int32_t retorno = -1;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
@@ -182,9 +202,11 @@ int muse_unmap(uint32_t dir) {
 		recibirInt(socketCliente, &retorno); //@return: error [-1] / ok [0]
 		close(socketCliente);
 		if(retorno != -1) {
-				loggearInfo("Archivo de mappeo eliminado con éxito");
+				sprintf(aux,"Información de %u desmappeada con éxito para el proceso %d",dir,id_muse);
+				loggearInfo(aux);
 			} else {
-				loggearWarning("No se ha podido eliminar el archivo de mappeo");
+				sprintf(aux,"No se ha podido eliminar el archivo de mappeado en %u para el proceso %d",dir,id_muse);
+				loggearWarning(aux);
 			}
 	}
 
@@ -194,21 +216,24 @@ int muse_unmap(uint32_t dir) {
 }
 
 void muse_close() {
-
+	char aux[150];
 	int32_t retorno = -1;
 	int socketCliente = levantarCliente(ip_muse, puerto_muse);
 
 	if(socketCliente != -1) {
-		loggearInfo("Cerrando la biblioteca MUSE...");
+		//loggearInfo("Cerrando la biblioteca MUSE...");
 		enviarClose(socketCliente, id_muse);
 		recibirInt(socketCliente, &retorno); //@return: error [-1] / ok [0]
 		close(socketCliente);
 		if(retorno != -1) {
-				loggearInfo("Biblioteca MUSE cerrada con éxito");
-				destruirLog();
+				sprintf(aux,"Biblioteca MUSE cerrada con éxito para el proceso %d",id_muse);
+				loggearInfo(aux);
+
 			} else {
-				loggearWarning("No se ha podido cerrar la biblioteca, servidor MUSE caido");
+				sprintf(aux,"No se ha podido cerrar la biblioteca para el proceso %d",id_muse);
+				loggearWarning(aux);
 			}
+		destruirLog();
 	}
 
 }
