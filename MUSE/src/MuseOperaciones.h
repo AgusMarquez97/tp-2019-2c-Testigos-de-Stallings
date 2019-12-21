347eef3bcdce7a9cc103a3fe64cd7b74690603a9
AgusMarquez97 <Aguusmarquez@gmail.com>
2019-12-21 13:50:17 -0300

    Update MuseOperaciones.h

--- MUSE/src/MuseOperaciones.h
+++ MUSE/src/MuseOperaciones.h
@@ -3,6 +3,28 @@
 
 #include "MuseHeapMetadata.h"
 
+void loggearDireccion(char * idProceso, uint32_t posicionSegmento)
+{
+	t_list* segmentos;
+	t_segmento* segmento;
+
+	char aux[500];
+
+	pthread_mutex_lock(&mutex_diccionario);
+	segmentos = dictionary_get(diccionarioProcesos,idProceso);
+	segmento = obtenerSegmento(segmentos, posicionSegmento);
+	bool log(t_pagina * unaPagina)
+	{
+		int pagina = posicionSegmento/tamPagina;
+		return (pagina == unaPagina->nroPagina);
+	}
+	t_pagina * unaPagina = list_find(segmento->paginas,(void*)log);
+	sprintf(aux,"Para el proceso %s, segmento %d, se accede a la pagina %d (marco %d) con el offset %d",
+			idProceso,segmento->id_segmento,unaPagina->nroPagina,unaPagina->nroMarco,posicionSegmento%tamPagina);
+	loggearInfo(aux);
+	pthread_mutex_unlock(&mutex_diccionario);
+}
+
 int procesarHandshake(char* idProceso) {
 
 	char msj[200];
@@ -47,6 +69,8 @@ uint32_t procesarMalloc(char* idProceso, int32_t tamanio) {
 		loggearWarning(msj);
 	}
 
+	loggearDireccion(idProceso,posicionRetorno);
+
 	return posicionRetorno;
 
 }
@@ -56,7 +80,9 @@ int32_t procesarFree(char* idProceso, uint32_t posicionSegmento) {
 	char msj[200];
 
 	if(poseeSegmentos(idProceso)) {
-		return analizarFree(idProceso,posicionSegmento);
+		loggearDireccion(idProceso,posicionSegmento);
+		int ret =  analizarFree(idProceso,posicionSegmento);
+		return ret;
 	}
 
 	sprintf(msj, "[pid|%s]-> No realizo el init correspondiente", idProceso);
@@ -71,6 +97,7 @@ void* procesarGet(char* idProceso, uint32_t posicionSegmento, int32_t tamanio) {
 	char msj[200];
 
 	if(poseeSegmentos(idProceso)) {
+		loggearDireccion(idProceso,posicionSegmento);
 		return analizarGet(idProceso, posicionSegmento,tamanio);
 	}
 
@@ -86,7 +113,9 @@ int procesarCpy(char* idProceso, uint32_t posicionSegmento, int32_t tamanio, voi
 	char msj[200];
 
 	if(poseeSegmentos(idProceso)) {
-		return analizarCpy(idProceso, posicionSegmento, tamanio, contenido);
+		int ret = analizarCpy(idProceso, posicionSegmento, tamanio, contenido);
+		loggearDireccion(idProceso,posicionSegmento);
+		return ret;
 	}
 
 	sprintf(msj, "[pid|%s]-> No realizo el init correspondiente", idProceso);
@@ -101,7 +130,9 @@ uint32_t procesarMap(char* idProceso, char* path, int32_t tamanio, int32_t flag)
 	char msj[200];
 
 	if(existeEnElDiccionario(idProceso)) {
-		return analizarMap(idProceso, path, tamanio, flag);
+		int ret = analizarMap(idProceso, path, tamanio, flag);
+		loggearDireccion(idProceso,ret);
+		return ret;
 	}
 
 	sprintf(msj, "[pid|%s]-> No realizo el init correspondiente", idProceso);
@@ -116,6 +147,7 @@ int procesarSync(char* idProceso, uint32_t posicionSegmento, int32_t tamanio) {
 	char msj[200];
 
 	if(existeEnElDiccionario(idProceso)) {
+		loggearDireccion(idProceso,posicionSegmento);
 		return analizarSync(idProceso, posicionSegmento, tamanio);
 	}
 
@@ -132,6 +164,7 @@ int procesarUnmap(char* idProceso, uint32_t posicionSegmento) {
 	char msj[200];
 
 	if(existeEnElDiccionario(idProceso)) {
+		loggearDireccion(idProceso,posicionSegmento);
 		return analizarUnmap(idProceso, posicionSegmento);
 	}
 
